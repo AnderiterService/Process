b6694bec182baa53a823bde102b8b63a1040a763
shift-lab-kuznetsov <e.kuznetsov1@g.nsu.ru>
2023-11-24 21:34:31 +0700

    Initial commit

---
+++ src/main/java/com/changellenge/hackaton/sber/changellengesber/ChangellengeSberApplication.java
@@ -0,0 +1,13 @@
+package com.changellenge.hackaton.sber.changellengesber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChangellengeSberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChangellengeSberApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/changellenge/hackaton/sber/changellengesber/ChangellengeSberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.changellenge.hackaton.sber.changellengesber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChangellengeSberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
